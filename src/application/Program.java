27069d184e274792dd2056592e6207006cb1420c
joabe <joabe.18@hotmail.com>
2022-09-25 12:59:49 -0300

    Checkmate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,7 @@ public class Program {
 		ChessMatch match = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		 
-		while (true) {
+		while (!match.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(match, captured);
@@ -48,6 +48,8 @@ public class Program {
 				sc.nextLine();
 			}
 		}
+		UI.clearScreen();
+		UI.printMatch(match, captured);
 	}
 
 }
